307b97c96af17a416d5c6864d38b4474297ea193
UXNation <uxnation.inc@gmail.com>
2022-03-19 21:48:17 +0530

    console statements removed

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
 require('dotenv').config({path:'.env'});
 //const express = require('express');
-const {app, express, router}= require('./connection');
-const userModel = require('./models/user.model.js');
-const iceItemModel = require('./models/iceitem.model.js');
-const cartModel = require('./models/cart.model')
-const productCategoryModel = require('./models/productCategory.model.js');
-const path = require('path');
+const {app, router}= require('./connection');
 
 const multer = require('multer');
-
-const { response } = require('express');
-const bcrypt = require('bcryptjs');
 
 //ROUTEs ---------------dwdw
 const loginUser = require('./routes/loginUser');
@@ -18,7 +10,6 @@ const registerUser = require('./routes/registerUser');
 const googleLogin = require('./routes/googleLogin');
 const authCheck = require('./routes/authCheck');
 const retrieveCategories = require('./routes/retrieveCategories');
-//const myModule = require('./routes/saveIceItem');
 const saveIceItem = require('./routes/saveIceItem');
 const updateUserDetails = require('./routes/updateUserDetails');
 const retrieveProducts = require('./routes/retrieveProducts');
@@ -29,11 +20,6 @@ const deleteFromCart = require('./routes/deleteFromCart');
 const getLoggedInUserDetails = require('./routes/getLoggedInUserDetails');
 const saveCartItemsToAdmin = require('./routes/saveOrderToAdmin');
 
-// const router = express.Router();
-// const app = express();
-// .use(express.json());
-// app.use(cors());
-
  router.get('/',(req, res)=>{
     res.send('hello world');
 })
@@ -43,7 +29,6 @@ const saveCartItemsToAdmin = require('./routes/saveOrderToAdmin');
    router.use('/api/', loginUser);
    router.use('/api/', registerUser);
    router.use('/api/',googleLogin);
-  // router.use('/api/', myModule.saveIceItem);
    router.use('/api/', saveIceItem);
    router.use('/api/', retrieveCategories);
    router.use('/api/', retrieveProducts);
